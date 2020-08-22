5d4fabb083da94683a119e2008e80302659c143b
zstcs <zstcs@sina.com>
2020-08-22 12:29:56 +0800

    fixed:添加非当前tab项类型为 keepalive时移出时未能从keepalive移出的问题

--- src/components/head/head-tabs.vue
+++ src/components/head/head-tabs.vue
@@ -2,13 +2,13 @@
   <div :class="myClass">
     <a-tabs
       v-model="activeKey"
-      :hideAdd="hideAdd"
       :class="headTabClass"
+      :hideAdd="hideAdd"
       :tabBarGutter="tabBarGutter"
       type="editable-card"
       @change="handleChange"
-      @tabClick="handleTabClick"
       @edit="handelEdit"
+      @tabClick="handleTabClick"
     >
       <a-tab-pane v-for="item in menus" :key="item.key" :closable="closable && menus.length > 1">
         <template v-slot:tab>
@@ -69,94 +69,115 @@ export default {
       default: null
     }
   },
-  data() {
+  data () {
     return {
       menus: [],
-      activeKey: 0
+      activeKey: 0,
+      tempKeep: {}
     }
   },
   computed: {
     ...mapGetters(['showAvatar', 'allowCollapsed']),
-    headTabClass() {
+    headTabClass () {
       return { 'head-tabs': true }
     },
     //是否允许关闭当前tabPane
-    allowClosableTab() {
+    allowClosableTab () {
       return this.closable && this.menus.length > 1
     },
-    myClass() {
+    myClass () {
       return {
         collapsed: this.allowCollapsed
       }
     }
   },
   watch: {
-    defaultKey(c, o) {
+    defaultKey (c, o) {
       if (c !== o) {
         this.activeKey = c
       }
     },
-    items(c, o) {
+    items (c, o) {
       if (c !== o) {
         this.menus = c || []
       }
     },
-    $route(c, o) {
-      // 移出tab时，需要从keepAlive 里，移出对应的页面缓存
-      if (this._is_remove_tab_ && o.meta && o.meta.keepAlive) {
-        const len = o.matched.length
-        let m = o.matched[len - 1]
-        if (m && (m = m.instances) && (m = m.default)) {
-          if (!m.$vnode.parent && m.$parent.$options.name === 'BlankLayout') {
-            m = m.$parent
-          }
-          let $v = m.$vnode
-          // 当前vnode存在父级对象，且父级对象拥有cache
-          if ($v.parent && $v.parent.componentInstance && $v.parent.componentInstance.cache) {
-            const cOptions = $v.componentOptions
-            const cache = $v.parent.componentInstance.cache || {}
-            const keys = $v.parent.componentInstance.keys || []
-            let key =
-              $v.componentOptions.Ctor.cid + ($v.componentOptions.tag ? `::${this.$vnode.componentOptions.tag}` : '')
-            $v.key && (key = $v.key)
-            keys.find((m, index) => {
-              if (m === key) {
-                keys.splice(index, 1)
-                return true
+    $route: {
+      handler (c, o) {
+        // 移出tab时，需要从keepAlive 里，移出对应的页面缓存
+        if (o.meta && o.meta.keepAlive) {
+          const len = o.matched.length
+          let m = o.matched[len - 1]
+          if (m && (m = m.instances) && (m = m.default)) {
+            if (!m.$vnode.parent && m.$parent.$options.name === 'BlankLayout') {
+              m = m.$parent
+            }
+            let $v = m.$vnode
+            // 当前vnode存在父级对象，且父级对象拥有cache
+            if ($v.parent && $v.parent.componentInstance && $v.parent.componentInstance.cache) {
+              const cOptions = $v.componentOptions
+              const cache = $v.parent.componentInstance.cache || {}
+              const keys = $v.parent.componentInstance.keys || []
+              let key =
+                $v.componentOptions.Ctor.cid + ($v.componentOptions.tag ? `::${this.$vnode.componentOptions.tag}` : '')
+              $v.key && (key = $v.key)
+              keys.find((m, index) => {
+                if (m === key) {
+                  keys.splice(index, 1)
+                  return true
+                }
+              })
+              if (this._is_remove_tab_) {
+                cache[key] && Reflect.deleteProperty(cache, key)
+                this.tempKeep[o.name] && Reflect.deleteProperty(this.tempKeep, o.name)
+              } else {
+                this.tempKeep[o.name] = { cache, key, keys }
               }
-            })
-            cache[key] && Reflect.deleteProperty(cache, key)
+            }
           }
         }
-      }
-      this._is_remove_tab_ = false
+        this._is_remove_tab_ = false
+
+      },
+      immediate: true
     }
   },
-  created() {
+  created () {
     this.menus = this.items || []
     this.activeKey = this.defaultKey
     this._is_remove_tab_ = false
   },
   methods: {
-    handleChange(activeKey) {
+    handleChange (activeKey) {
       if (this.tabSelect) {
         this.tabClick(this.menus.find(m => m.key === activeKey))
       } else {
         this.$emit('tabSelect', this.menus.find(m => m.key === activeKey))
       }
     },
-    handleTabClick(activeKey) {
+    handleTabClick (activeKey) {
       if (this.tabClick) {
         this.tabClick(this.menus.find(m => m.key === activeKey))
       } else {
         this.$emit('tabClick', this.menus.find(m => m.key === activeKey))
       }
     },
-    handelEdit(targetKey, action) {
+    handelEdit (targetKey, action) {
       this[action](targetKey)
+      if (!this.tempKeep[targetKey]) return
+      const { key, cache, keys } = this.tempKeep[targetKey]
+      cache[key] && Reflect.deleteProperty(cache, key)
+      keys.find((m, idx) => {
+        if (m === key) {
+          keys.splice(idx, 1)
+          return true
+        }
+
+      })
+
     },
     /**移出tab项 */
-    remove(targetKey) {
+    remove (targetKey) {
       //当tab项只有一个不能进行移出操作
       if (this.menus.length === 1) return
       const length = this.menus.length
@@ -172,13 +193,14 @@ export default {
           const nextIndex = this.menus.findIndex(m => m.key === targetKey) + 1
           tempActiveKey = this.menus[nextIndex].key
         }
+        this._is_remove_tab_ = true
       }
       //移出tab
       const removeItem = this.menus.find(m => m.key === targetKey)
       this.menus = this.menus.filter(m => m.key !== targetKey)
       this.activeKey = tempActiveKey
       this.handleChange(this.activeKey)
-      this._is_remove_tab_ = true
+
       this.$emit('update:items', this.menus)
       if (this.tabRemove) {
         this.tabRemove(removeItem)
