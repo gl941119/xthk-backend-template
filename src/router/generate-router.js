0f1f888b60b607810d8d38db0659062c4aa08077
张松涛 <2367476520@qq.com>
2020-12-10 19:12:13 +0800

    fix(主框架): 修正路由对象加载的处理

--- src/router/generate-router.js
+++ src/router/generate-router.js
@@ -2,7 +2,7 @@
 const whiteList = ['./index.js', './generate-router.js', './permission.js', './mock-router.js']
 const allRouter = require.context('.', false, /\.js$/)
 if (process.env.NODE_ENV === 'development') {
-  whiteList.splice(2)
+  whiteList.splice(3)
 }
 const routers = allRouter.keys().filter(key => !whiteList.includes(key)).map(key => {
   return allRouter(key).default[0]
